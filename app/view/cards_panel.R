b3750be082b68b7f7ad8f2ab003836f340d51c19
Erika Borelli <erikasoaresm@hotmail.com>
2024-07-21 20:03:41 +0100

    initiated panel cards

--- app/view/cards_panel.R
+++ app/view/cards_panel.R
@@ -1,9 +1,11 @@
 # app/view/cards_panel.R
 
 box:use(
-  shiny[moduleServer, NS, tagList, actionButton, textInput],
+  shiny[moduleServer, NS, tagList, actionButton, textInput,
+        sliderInput],
   bslib[card, card_header, card_body, card_footer]
 )
+
 
 
 # UI ----------------------------------------------------------------------
@@ -12,7 +14,20 @@ box:use(
 #' @export
 ui <- function(id){
   ns <- NS(id)
-  tagList()
+  tagList(
+    card(
+      full_screen = TRUE,
+      card_header(
+        "This is the header"
+      ),
+      card_body(
+
+      ),
+      card_footer(
+        "This is the footer"
+      )
+    )
+  )
 }
 
 
@@ -25,3 +40,8 @@ server <- function(id) {
 
   })
 }
+
+# sliderInput("card_qt",
+#             label = h3("Choose cards quantity"),
+#             min = 0,
+#             max = 100, value = 50)
